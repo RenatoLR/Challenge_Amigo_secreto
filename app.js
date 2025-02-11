a42d76c8d5acbb8e369e95eae9ca5e743b4ccbef
Renato La Rosa <ing.rlarosa97@hotmail.com>
2025-02-11 16:02:40 -0500

    Function par almacenar amigos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Array para almacenar lista de amigos
+let Amigos = [];
+
+// Function par almacenar amigos
+function agregarAmigos () {
+    const InputAmigo = document.querySelector(`#amigo`);
+    const NombreAmigo = InputAmigo
+   
+    // Agregar el nombre a la matriz (Array)
+    Amigos.push(NombreAmigo)
+
+    // Verificar que el nombre ingresado no se repite
+    if (Amigos.includes(NombreAmigo)) {
+        alert (`${NombreAmigo} ya se encuentra registrado en la lista`)
+        return;
+    }
+
+    // Verificar que sno sea un espacio en blanco
+    if (Nombre === ""){
+        alert ("Por favor ingrese un nombre valido.")
+        return;
+    }
+
+    //Limpiar lista de amigos
+    InputAmigo.ariaValueMax = "";
+
+    //Actualizar lista en HTML
+    ActualizarLista ();
+} 
+agregarAmigos ()
+// Function para la visualización en interface
+
+
+// Function sorteo de amigo secreto
